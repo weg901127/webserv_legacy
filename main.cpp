227214f055e0aeef411a64f4c7782721a8682883
gilee <gilee@student.42seoul.kr>
2022-04-18 20:42:50 +0900

    lol

--- main.cpp
+++ main.cpp
@@ -6,9 +6,18 @@
 #include "options.hpp"
 #include <netdb.h>
 #include <csignal>
+#include <sys/fcntl.h>
+#include <sys/stat.h>
+
 #define MAX_BACKLOG 5
 #define DEFAULT_PORT "80"
 #define LINE_BUF_SIZE 100
+#define TIME_BUF_SIZE 64
+#define BLOCK_BUF_SIZE 100
+#define HTTP_MINOR_VERSION 1
+#define SERVER_NAME "FT_WEBSERV"
+#define SERVER_VERSION "0.1"
+
 typedef void (*sighandler_t)(int);
 
 struct FileInfo {
@@ -144,25 +153,90 @@ static struct HTTPRequest* read_request(FILE *in) {
     }
     req->length = content_length(req);
 }
-//TODO 시1발 존나기네!!
+
+static char * build_fspath(char *docroot, char *urlpath) {
+	char *path;
+
+	path = (char *)malloc(strlen(docroot) + 1 + strlen(urlpath) + 1);
+	sprintf(path, "%s/%s", docroot, urlpath);
+	return path;
+}
+
+static struct FileInfo* get_fileinfo(char *docroot, char *urlpath) {
+	struct FileInfo *info;
+	struct stat st;
+
+	info = new struct FileInfo;
+	info->path = build_fspath(docroot, urlpath);
+	info->ok = 0;
+	if (lstat(info->path, &st) < 0) return info;
+	if (!S_ISREG(st.st_mode)) return info;
+	info->ok = 1;
+	info->size = st.st_size;
+	return info;
+}
+
+static void output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status) {
+	time_t t;
+	struct tm   *tm;
+	char buf[TIME_BUF_SIZE];
+
+	t = time(NULL);
+	tm = gmtime(&t);
+	if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
+	strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
+	fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
+	fprintf(out, "Date: %s\r\n", buf);
+	fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+	fprintf(out, "Connection: close\r\n");
+}
+
 static void do_file_response(struct HTTPRequest *req, FILE  *out, char *docroot) {
     struct FileInfo *info;
 
     info = get_fileinfo(docroot, req->path);
     if (!info->ok) {
-
+		//free_fileinfo(docroot, req->path);
+		//not_found(req, out);
+		return ;
     }
+	output_common_header_fields(req, out, "200 OK");
+	fprintf(out, "Content-Length: %ld\r\n", info->size);
+	//fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+	fprintf(out, "Content-Type: %s\r\n", "text/plain");
+	fprintf(out, "\r\n");
+	if (strcmp(req->method, "HEAD") != 0) {
+		int fd;
+		char buf[BLOCK_BUF_SIZE];
+		ssize_t n;
+
+		fd = open(info->path, O_RDONLY);
+		if (fd < 0)
+			log_exit("failed to open %s: %s", info->path, strerror(errno));
+		for (;;) {
+			n = read(fd, buf, BLOCK_BUF_SIZE);
+			if (n < 0)
+				log_exit("failed to read %s: %s", info->path, strerror(errno));
+			if (n == 0)
+				break;
+			if (fwrite(buf, n, 1, out) < n)
+				log_exit("failed to write to socket: %s", strerror(errno));
+		}
+		close(fd);
+	}
+	fflush(out);
+	//free_fileinfo(info);
 }
 
 static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
-//    if (strcmp(req->method, "GET") == 0)
-//        do_file_response(req, out, docroot);
-//    else if (strcmp(req->method, "HEAD") == 0)
-//        do_file_response(req, out, docroot);
+    if (strcmp(req->method, "GET") == 0)
+        do_file_response(req, out, docroot);
+    else if (strcmp(req->method, "HEAD") == 0)
+        do_file_response(req, out, docroot);
 //    else if (strcmp(req->method, "POST") == 0)
-//        method_not_allowed(req, out);
+//        //method_not_allowed(req, out);
 //    else
-//        not_implemented(req, out);
+//        //not_implemented(req, out);out
 }
 
 static void service(FILE *in, FILE *out, char *docroot) {
@@ -195,7 +269,7 @@ static int listen_socket(char *port) {
         freeaddrinfo(res);
         return sock;
     }
-    //log_exit("failed to listen socket");
+    log_exit("failed to listen socket");
     return -1;
 }
 
@@ -241,14 +315,14 @@ static void server_main(int server_fd, char *docroot) {
 
         sock = accept(server_fd, (struct sockaddr*)&addr, &addrlen);
         if (sock < 0)
-            //log_exit("accept(2) failed:");
+            log_exit("accept(2) failed:");
         pid = fork();
         if (pid < 0) exit(3);
         if (pid == 0) {
             FILE *inf = fdopen(sock, "r");
             FILE *outf = fdopen(sock, "w");
 
-            //service(inf, outf, docroot);
+            service(inf, outf, docroot);
             exit(0);
         }
         close(sock);
@@ -256,6 +330,7 @@ static void server_main(int server_fd, char *docroot) {
 }
 
 int main(int argc, char **argv) {
+	/*
     int server_fd;
     char *port = NULL;
     char *docroot;
@@ -297,6 +372,7 @@ int main(int argc, char **argv) {
 //        setup_environment(docroot, user, group);
 //        docroot = "";
 //    }
+
     install_signal_handlers();
     server_fd = listen_socket(port);
     //if (!debug_mode) {
@@ -304,5 +380,12 @@ int main(int argc, char **argv) {
     //    become_daemon();
     //}
     server_main(server_fd, docroot);
+    */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+		exit(1);
+	}
+	install_signal_handlers();
+	service(stdin, stdout, argv[1]);
     return 0;
 }
